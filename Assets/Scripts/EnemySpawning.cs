05856b639cab12b22048b64079b75df80f56b2e5
Yavor <159168653+YavorArabadjiev@users.noreply.github.com>
2024-07-22 12:43:22 +0300

    fixed issue with player being destroyed

--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -29,13 +29,7 @@ public class EnemySpawning : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            Destroy(collision.gameObject);
-        }
-    }
+    
 
 
 }
