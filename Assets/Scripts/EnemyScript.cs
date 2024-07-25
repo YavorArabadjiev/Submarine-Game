2ca3f769f5622b373b1134ca32678c6807fe3d64
Yavor <159168653+YavorArabadjiev@users.noreply.github.com>
2024-07-25 11:15:28 +0300

    Added cooldown when player collides with enemy.
    While in cooldown, the player doesn't take damage from enemies. I also visualized the amount of health the player has with a number in the corner of the screen.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,25 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class EnemyScript : MonoBehaviour
 {
     GameObject player;
+    
     [SerializeField] float speed = 5f;
     [SerializeField] int healthPoints = 50;
+    [SerializeField] int playerHealthPoints = 3;
+    [SerializeField] float playerSaveTimeSeconds = 1.5f;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");            
     }
 
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        if(playerHealthPoints <= 0)
+        {
+            Destroy(player);
+        }
+
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,4 +40,6 @@ public class EnemyScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,6 @@
+using System.Collections;
 using UnityEngine;
-
+using TMPro;
 public class PlayerScript : MonoBehaviour
 {
 
@@ -7,14 +8,24 @@ public class PlayerScript : MonoBehaviour
     float yInput;
     [SerializeField] float moveSpeed = 4f;
     Vector3 moveVector;
+    [SerializeField] int healthPoints = 3;
+    [SerializeField] GameObject[] enemy;
+    [SerializeField] float playerSaveTimeSeconds = 1.5f;
+    [SerializeField] TextMeshProUGUI playerHealthText;
     void Start()
     {
-        
+        enemy = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     void Update()
     {
-        
+        playerHealthText.text = healthPoints.ToString();
+
+        if(healthPoints <= 0)
+        {
+            gameObject.transform.DetachChildren();
+            Destroy(gameObject);
+        }
     }
 
     private void FixedUpdate()
@@ -22,7 +33,7 @@ public class PlayerScript : MonoBehaviour
         MovePlayer();
     }
 
-    void MovePlayer() 
+    void MovePlayer()
     {
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
@@ -32,10 +43,29 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
-            gameObject.transform.DetachChildren();
-            Destroy(gameObject);
+            //gameObject.transform.DetachChildren();
+            //Destroy(gameObject);
+
+            healthPoints -= 1;
+            StartCoroutine(safeTime());
+
+            IEnumerator safeTime()
+            {
+                //Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), enemy[0].GetComponent<Collider2D>());
+                //Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), enemy[1].GetComponent<Collider2D>());
+                if(collision.gameObject.tag == "Enemy")
+                {
+                    healthPoints -= 0;
+                }
+                yield return new WaitForSeconds(playerSaveTimeSeconds);
+                //Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), enemy[0].GetComponent<Collider2D>(), false);
+                //Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), enemy[1].GetComponent<Collider2D>(), false);
+            }
+
+            
         }
     }
+
 }
