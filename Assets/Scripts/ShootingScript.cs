b3e1354f9336ec1fdd6d73758957ce88eeae4bbe
Yavor <159168653+YavorArabadjiev@users.noreply.github.com>
2024-09-06 15:54:36 +0300

    Weapon reloading and amount of bullets upgrade

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShootingScript : MonoBehaviour
@@ -6,6 +8,13 @@ public class ShootingScript : MonoBehaviour
     [SerializeField] GameObject[] bullet;
     [SerializeField] int level = 1;
     [SerializeField] GameObject upgradeMenu;
+    int bulletsShot = 0;
+    int bulletCount = 4;
+    bool canshoot = true;
+    float timer = 0;
+    float reloadTime = 2f;
+    int bulletAmountLevel = 0;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -18,8 +27,9 @@ public class ShootingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && level == 1)
+        if (Input.GetButtonDown("Fire1") && level == 1 && bulletsShot < bulletCount)
         {
+            bulletsShot++;
             Instantiate(bullet[0], transform.position, Quaternion.identity);
         }
         if(level >= 2)
@@ -31,14 +41,32 @@ public class ShootingScript : MonoBehaviour
                 
         }
 
+        if(bulletsShot >= bulletCount)
+        {
+            timer += Time.deltaTime;
+            if(timer > reloadTime)
+            {
+                bulletsShot = 0;
+                timer = 0;
+            }
+        }
         
     }
 
     public void BulletLevelUpgrade()
     {
-        level = level + 1;
+        level += 1;
         upgradeMenu.SetActive(false);
         Time.timeScale = 1.0f;
+    }
+
+    public void BulletAmountUpgrade()
+    {
+        bulletAmountLevel += 1;
+        upgradeMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        if(bulletAmountLevel == 1)
+        bulletCount += 1;
     }
 }
 
